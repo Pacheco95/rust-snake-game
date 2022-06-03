171c174f4e87656887569ce4c6dc2b89483e2818
Michael Pacheco <michael.dias@luizalabs.com>
2022-06-03 13:14:04 -0300

    feat: event handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 pub mod engine {
     use std::time::Duration;
 
-    use sdl2::{EventPump, Sdl};
+    use sdl2::{Sdl};
     use sdl2::event::Event;
     use sdl2::keyboard::Keycode;
     use sdl2::pixels::Color;
     use sdl2::render::WindowCanvas;
+    use vecmath::Vector2;
 
     fn get_canvas(sdl_context: &Sdl) -> WindowCanvas {
         let video_subsystem = sdl_context.video().unwrap();
@@ -42,23 +43,21 @@ pub mod engine {
         }
 
         fn handle_event(&mut self, event: Event) {
-            let mut direction;
-
             if let Event::KeyDown {keycode, ..} = event {
-                direction = match keycode {
-                    Some(Keycode::Up) => (0, 1),
-                    Some(Keycode::Down) => (0, -1),
-                    Some(Keycode::Left) => (-1, 0),
-                    Some(Keycode::Right) => (1, 0),
-                    _ => (0, 0)
-                }
+                let _direction: Vector2<i32> = match keycode {
+                    Some(Keycode::Up) => [0, 1],
+                    Some(Keycode::Down) => [0, -1],
+                    Some(Keycode::Left) => [-1, 0],
+                    Some(Keycode::Right) => [1, 0],
+                    _ => [0, 0]
+                };
             }
         }
 
         pub fn run(&mut self) {
-            'game_loop: loop {
-                let mut event_pump = self.context.event_pump().unwrap();
+            let mut event_pump = self.context.event_pump().unwrap();
 
+            'game_loop: loop {
                 for event in event_pump.poll_iter() {
                     match event {
                         Event::Quit { .. }
